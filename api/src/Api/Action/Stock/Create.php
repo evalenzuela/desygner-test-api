2b0b76343938d00fde910c60bedbdef883db2f13
Eduardo Valenzuela <valenzuela.eduardo@gmail.com>
2022-08-29 12:07:58 -0500

    Stock create service

--- api/src/Api/Action/Stock/Create.php
+++ api/src/Api/Action/Stock/Create.php
@@ -2,6 +2,22 @@
 
 namespace App\Api\Action\Stock;
 
+use App\Entity\Stock;
+use App\Service\Stock\StockCreateService;
+use Symfony\Component\HttpFoundation\Request;
+
 class Create
 {
+
+    private StockCreateService $stockCreateService;
+
+    public function __construct(StockCreateService $stockCreateService)
+    {
+        $this->stockCreateService = $stockCreateService;
+    }
+
+    public function __invoke(Request $request): Stock
+    {
+        return $this->stockCreateService->create($request);
+    }
 }
